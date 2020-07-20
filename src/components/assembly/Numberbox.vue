6e0ecad0d547b3056c15c55bcaa2b2946666fb41
greathu <1769811019@qq.com>
2020-07-20 21:56:53 +0800

    第二次修改

--- src/components/assembly/Numberbox.vue
+++ src/components/assembly/Numberbox.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="mui-numbox" data-numbox-min="1" data-numbox-max="9">
     <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-    <input id="test" class="mui-input-numbox" type="number" value="5" />
+    <input
+      id="test"
+      class="mui-input-numbox"
+      type="number"
+      value="5"
+      @change="countChanged()"
+      ref="numbox"
+    />
     <button class="mui-btn mui-btn-numbox-plus" type="button" disabled="">
       +
     </button>
@@ -13,6 +20,14 @@ import mui from "../../lib/mui/js/mui.min.js";
 export default {
   mounted() {
     mui(".mui-numbox").numbox();
+  },
+  methods: {
+    countChanged() {
+      /*      console.log(this.$refs.numbox.values);
+      获取的数值，在向父组件传递
+       */
+      this.$emit("getcount", parseInt(5));
+    }
   }
 };
 </script>
